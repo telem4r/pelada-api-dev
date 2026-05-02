ab55468da3e89248d7e71227857630dfce4e4847
Ewerton Oliveira <ewerton.oliveria@gmail.com>
2026-05-02 19:00:39 +0100

    Delete api/app/services/group_ranking_service.py

--- api/app/services/group_ranking_service.py
+++
@@ -1,9 +0,0 @@
-
-from uuid import UUID
-
-def get_group_ranking(group_id: UUID):
-    # TODO: replace with real query
-    return [
-        {"player_id": "1", "name": "Player 1", "score": 100, "position": 1},
-        {"player_id": "2", "name": "Player 2", "score": 80, "position": 2},
-    ]
